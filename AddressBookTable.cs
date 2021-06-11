2c0baaf9c4f686a6142667dee25161aa22718fb8
unknown <jimpidas>
2021-06-11 16:30:34 +0530

    [Jyosmita]Added: first,last name,address,city,zip,phone number as attributes

--- AddressBookTable.cs
+++ AddressBookTable.cs
@@ -1,10 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Text;
 
 namespace AddressBookService_LINQ_And_DataTable
 {
     class AddressBookTable
     {
+        public void CreateAddressBookDataTable()
+        {
+            DataTable addressBookTable = new DataTable();
+            addressBookTable.Columns.Add("FirstName", typeof(string));
+            addressBookTable.Columns.Add("LastName", typeof(string));
+            addressBookTable.Columns.Add("Address", typeof(string));
+            addressBookTable.Columns.Add("City", typeof(string));
+            addressBookTable.Columns.Add("State", typeof(string));
+            addressBookTable.Columns.Add("Zip", typeof(int));
+            addressBookTable.Columns.Add("PhoneNumber", typeof(long));
+            addressBookTable.Columns.Add("Email", typeof(string));
+        }
     }
 }
